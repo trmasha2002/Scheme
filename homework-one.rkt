873e2ca6aaf0e46c7f90bffd21940a07101d6f92
trmasha2002 <33479104+trmasha2002@users.noreply.github.com>
2020-09-06 17:32:00 +0300

    Update homework-one.rkt

--- homework-one.rkt
+++ homework-one.rkt
@@ -1,3 +1,14 @@
+(define (day_of_week d m y)
+  (if (> m 2) 
+    (remainder (- (+ (+ (+ (+ d (quotient (* 31 (- m 2)) 12)) y) (quotient y 4)) (quotient y 400)) (quotient y 100)) 7)
+    (remainder (- (+ (+ (+ (+ d (quotient (* 31 (+ m 10)) 12)) (- y 1)) (quotient (- y 1) 4)) (quotient (- y 1) 400)) (quotient (- y 1) 100)) 7)))
+(day_of_week 1 1 2020)
+(day_of_week 1 2 2016)
+(day_of_week 04 12 1975)
+(day_of_week 04 12 2006)
+
+
+
 (define (sqrt_solution a b c)
   (if (and (= a 0) (= b 0) (= c 0)) (list 1e18)
   (if (and (= a 0) (= b 0)) (list)
@@ -16,6 +27,8 @@
 (define (get_second_root a b c)
   (/ (- (- b) (sqrt (discriminant a b c))) (* 2 a)))
 (sqrt_solution 2 4 2)
+
+
 
 (define (my-gcd a b)
    (if (= b 0) a
