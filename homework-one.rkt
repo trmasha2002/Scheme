e152385812ec168f9cc5ac51dd4bc7f51fb93b30
trmasha2002 <33479104+trmasha2002@users.noreply.github.com>
2020-09-06 16:50:47 +0300

    Update homework-one.rkt

--- homework-one.rkt
+++ homework-one.rkt
@@ -1,3 +1,22 @@
+(define (sqrt_solution a b c)
+  (if (and (= a 0) (= b 0) (= c 0)) (list 1e18)
+  (if (and (= a 0) (= b 0)) (list)
+  (if (= a 0) (list (/ (- c) b))
+  (if (= b 0) (list (/ (- c) (* a a)))
+  (if (= (discriminant a b c) 0) (list (get_first_root a b c))
+  (if (> (discriminant a b c) 0)
+      (list (get_first_root a b c) (get_second_root a b c))
+      (list))))))))
+(define (discriminant a b c)
+  (- (* b b) (* 4 a c)))
+
+(define (get_first_root a b c)
+   (/ (+ (- b) (sqrt (discriminant a b c))) (* 2 a)))
+
+(define (get_second_root a b c)
+  (/ (- (- b) (sqrt (discriminant a b c))) (* 2 a)))
+(sqrt_solution 2 4 2)
+
 (define (my-gcd a b)
    (if (= b 0) a
    (my-gcd b (remainder a b))))
